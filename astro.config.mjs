459495a2776bc393fb4d6844dbae31b4f55c0003
jod35 <jodestrevin@gmail.com>
2025-04-08 13:26:01 +0300

    add pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
+  },
+});
